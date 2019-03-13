742868a26802df80e2a980bad5e70f99597ececc
Ross Mckenzie <rossmckenzie11@gmail.com>
2019-03-13 12:30:21 +0000

    Can complete tasks, and save the completed tasks to a lisy in the task file

--- cltask/task.py
+++ cltask/task.py
@@ -12,7 +12,7 @@ def init_argparse():
     parser = argparse.ArgumentParser(description=app_description)
     parser.add_argument('command',
             help="Which command to run",
-            choices=["add", "done", "delete", "list"], default="list", nargs='?')
+            choices=["add", "done", "delete", "list", "completed"], default="list", nargs='?')
     parser.add_argument('input',
             help="The data that the command needs to work with", nargs='*')
     parser.add_argument('-p', '--priority',
@@ -50,18 +50,47 @@ def task_file_exists():
 def create_task_file():
     """This gets called when the task file does not yet exist"""
     with open(task_file_location, 'w') as f:
-        f.write('{"Example Task": 5}')
+        f.write('{"Example Task": 5, "COMPLETE" : []}')
 
-def list_tasks():
+def list_active_tasks():
     """List the tasks in order of priority, with some nice formatting"""
-    sorted_tasks = sorted(zip(task_dictionary.values(), task_dictionary.keys()))
-    print("\n    Active Tasks: ")
+    active_tasks = task_dictionary.copy()
+    #We need to make a copy so we can ignore the "COMPLETED" key in the dictionary
+    del active_tasks["COMPLETE"]
+    sorted_tasks = sorted(zip(active_tasks.values(), active_tasks.keys()))
+    print("\tActive Tasks: ")
     for index, task in enumerate(sorted_tasks, 1):
-        print('{i:>8}. {task:-<50}> priority {p} '.format('-', i=index, task=task[1]+' ', p=task[0]))
+        print('{i:>9}. {task:-<50}> priority {p} '.format('-', i=index, task=task[1]+' ', p=task[0]))
+
+def list_completed_tasks():
+    """List the tasks in order of priority, with some nice formatting"""
+    print("\tCompleted Tasks: ")
+    for index, task in enumerate(task_dictionary["COMPLETE"], 1):
+        print('{i:>9}. {task}'.format('-', i=index, task=task))
+
+def task_done(task_name):
+    """Marks tasks that contain 'task_name' in their name as completed"""
+    tasks_to_mark = []
+    for task in task_dictionary.keys():
+        if task.find(task_name) >= 0:
+            tasks_to_mark.append(task)
+    if len(tasks_to_mark) > 0:
+        print("\tDo you want to mark these tasks as completed?: ")
+        for task in tasks_to_mark:
+            print("\t -{}".format(task))
+        delete_confirmation = input("\n\ty/n: ")
+        if delete_confirmation == 'y':
+            for task in tasks_to_mark:
+                task_dictionary["COMPLETE"].append(task)
+                del task_dictionary[str(task)]
+            print("\n\tMarked as complete")
+    else:
+        print("\tNo tasks match '{}'".format(task_name))
 
 if __name__ == "__main__":
     #Parse arguments
     args = init_argparse()
+    user_input = ' '.join(args.input)
 
     #Load tasks if file exists, otherwise create it
     if task_file_exists():
@@ -69,19 +98,22 @@ if __name__ == "__main__":
     else:
         create_task_file()
 
+    print(' ')
     #Main body
     if args.command == "list":
         #List is the default functionality if no other commands given
-        list_tasks()
+        list_active_tasks()
+    if args.command == "completed":
+        list_completed_tasks()
     if args.command == "add":
         #Input is stored as a list because of the nargs. Here we convert it to a string split with spaces
-        task_name = ' '.join(args.input)
-        add_task(task_name, args.priority)
-        save_tasks(task_dictionary)
-        list_tasks()
+        add_task(user_input, args.priority)
+        list_active_tasks()
     if args.command == "done":
-        print("Done")
+        task_done(user_input)
     if args.command == "delete":
         print("Delete")
+
+    save_tasks(task_dictionary)
     print(' ')
 
